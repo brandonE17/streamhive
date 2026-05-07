6f740b2fda1100c4effdcce05a8dc8b6dd00465c
brandonE17 <6044720@mborijnland.nl>
2026-05-07 10:00:15 +0200

    database connectie succesvol

--- core/Database.php
+++ core/Database.php
@@ -1,17 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "streamhive";
+session_start();
+
+
+
+$host = "localhost";
+$port = 3307; 
+$user = "root";
+$pass = "";
+$dbname = "readyornot";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
+    echo "Databaseverbinding succesvol!";
+} catch (PDOException $e) {
+    echo "Fout bij verbinden: " . $e->getMessage();
 }
-
-
-?>
