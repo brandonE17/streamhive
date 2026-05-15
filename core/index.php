6daadc4e469b495df51019bf97a84664381614fb
brandonE17 <6044720@mborijnland.nl>
2026-05-15 11:10:29 +0200

    testen voor het opslaan voor het registratie scherm en sessies

--- core/index.php
+++ core/index.php
@@ -1,12 +1,20 @@
 <?php
+session_start();
 
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
+    $email = $_POST['email'];
+    $psw = $_POST['psw'];
+
+    $_SESSION['username'] = $email;
+
+    header("Location: Database.php"); 
+    exit;
+} 
+ 
 
 echo " u bent ingelogd als: " . $_SESSION['username'] . "<br>";
-
-
-
 
 
 
