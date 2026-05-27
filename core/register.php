7ed1a55383da5c03c9193b7eb868de6a53f88010
brandonE17 <6044720@mborijnland.nl>
2026-05-27 10:40:56 +0200

    de homepagina

--- core/register.php
+++ core/register.php
@@ -32,14 +32,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $error = 'Fout bij registratie: ' . $e->getMessage();
         }
     }
-}
+} 
 ?> 
 <!DOCTYPE html>
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registreren</title>
+    <title>Registreren</title> 
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
