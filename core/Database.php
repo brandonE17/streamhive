caace3fecfb0f2e08a5006593f9d776c60910693
brandonE17 <6044720@mborijnland.nl>
2026-06-25 23:04:46 +0200

    eindelijk alle rot xampp foutjes gefixt

--- core/Database.php
+++ core/Database.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $port = 3306;
 $user = "root"; 
 $pass = ""; 
-$dbname = "streamhive";  
+$dbname = "streamhive2";  
  
 try {
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
